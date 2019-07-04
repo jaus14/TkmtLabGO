57b25621ace6731930bf35c3fbec711746ed96e0
jaus <sekiyahikarisi@yahoo.co.jp>
2019-07-04 18:11:30 +0900

    create user add page

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get "raids" => "raids#index"
   get "users" => "users#index"
+  get "user/new" => "users#new"
+  post "user/create" => "users#create"
 end
