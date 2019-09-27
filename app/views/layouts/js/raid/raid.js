fbb2fbf15e05fabad01e60354f78b7f745ed387c
tuboniki <6318517@ed.tus.ac.jp>
2019-09-27 17:47:58 +0900

    bityousei

---
+++ app/views/layouts/js/raid/raid.js
@@ -0,0 +1,15 @@
+
+
+function seni(){
+    /*
+    var name = document.form1.namae;
+    var passwd = document.form2.passwd;
+    
+    if(name == nameCheck() && passwd == passwdCheck()){
+
+    }else{
+
+    }*/
+
+    window.location.href = "login.html";
+}
