b42dcd193d05226a2e1abc7d8ca30592b8ca9e93
Masato Nozawa <tener18work@gmail.com>
2019-11-06 18:54:51 +0900

    メインページを統合した

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,8 +12,9 @@ class UsersController < ApplicationController
 
   def create
       @user = User.new(user_params)
+      @user.is_login = 0
       if @user.save
-          redirect_to users_path
+          redirect_to login_path
       else
           render 'new'
       end
