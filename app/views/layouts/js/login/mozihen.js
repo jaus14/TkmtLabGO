f0e735469ed9f93621562b24c14964b404c9d382
tuboniki <6318517@ed.tus.ac.jp>
2019-11-13 17:50:30 +0900

    ok

--- app/views/layouts/js/login/mozihen.js
+++ app/views/layouts/js/login/mozihen.js
@@ -1,26 +1,4 @@
-<<<<<<< HEAD
-$(function(){
-    $('#test').click(function () {
-      $(this).text("クリックされました");
-    });
-  });
 
-function mover(){
-    document.value = "POKEMON GO"
-}
-
-function seni(){
-    var name = document.form1.namae;
-    var passwd = document.form2.passwd;
-    /*
-    if(name == nameCheck() && passwd == passwdCheck()){
-
-    }else{
-
-    }*/
-
-    window.location.href = "raid.html";
-=======
 $(function(){
     $('#test').click(function () {
       $(this).text("クリックされました");
@@ -49,5 +27,4 @@ function check(name,passwd){
   //名前とパスワードが一致しているかどうか確認する処理
   
   return true;
->>>>>>> 8e42484ac0151e94fe72dbfaee843efcda47dae3
 }
